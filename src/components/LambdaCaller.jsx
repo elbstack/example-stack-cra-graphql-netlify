2891821c2b619773eee910dac0cbb466f3f9bbc8
Ansgar Mertens <a@amrtns.com>
2019-01-21 19:37:24 +0100

    use input from input

--- src/components/LambdaCaller.jsx
+++ src/components/LambdaCaller.jsx
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 class LambdaCaller extends Component {
+
+  inputRef = createRef();
+
+  handleClick = () => {
+    fetch(`.netlify/functions/example?msg=${this.inputRef.current.value}`).then(console.log);
+  };
+
   render() {
     return (
       <div className="mb-4 border-blue border-b-4">
         <div className="ml-4 pl-4 border-l-2 border-blue-light mb-4">
           <p className="text-2xl mb-2">Bernd Buzzer</p>
           <div className="flex items-center">
-            <input type="text" className="w-64 h-8 text-xl bg-white border border-blue-lighter mr-4" />
-            <p className="bg-blue text-white px-4 py-2 rounded-lg cursor-pointer" onClick={() => {
-              window.fetch('.netlify/functions/example').then(console.log);
-            }}>Buzz</p>
+            <input ref={this.inputRef} type="text" className="w-64 h-8 text-xl bg-white border border-blue-lighter mr-4" />
+            <p className="bg-blue text-white px-4 py-2 rounded-lg cursor-pointer" onClick={this.handleClick}>Buzz</p>
           </div>
 
         </div>
