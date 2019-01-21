e8c019370b52d9c093caabc426f431586ccc52bc
bers <bernd.strehl@netlight.com>
2019-01-21 18:40:04 +0100

    add interesting function with build

--- functions/example.js
+++ functions/example.js
@@ -1,8 +1,14 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-  statusCode: 200,
-  body: {
-    lucky: Math.random() > 0.5
-  }
-  });
-}
+import fetch from "node-fetch";
+
+const API_ENDPOINT =
+  "https://maker.ifttt.com/trigger/send_message/with/key/c6d5VNClNcsUl_Lk5qOPPo";
+
+exports.handler = async (event, context) => {
+  const message = event.queryStringParameters.msg || "Hello World";
+  return fetch(API_ENDPOINT+`?value1=${message}`)
+    .then(data => ({
+      statusCode: 200,
+      body: `fired`
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
