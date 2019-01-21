0a1cb17a359c6e69524187640a76f30d8669efbc
bers <bernd.strehl@netlight.com>
2019-01-21 19:24:06 +0100

    add caller component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import TrainViewer from './components/TrainViewer';
+import LambdaCaller from './components/LambdaCaller';
 
 class App extends Component {
   render() {
     return (
       <div className="w-screen h-screen bg-blue p-32 flex justify-center">
-        <div className="w-128 bg-grey-lightest p-8 shadow">
-          <h1>Example App</h1>
+        <div className="w-128 bg-grey-lightest shadow flex flex-col">
+          <h1 className="mb-4 p-4 border-b-4 border-blue">Demo App</h1>
           <TrainViewer/>
+          <LambdaCaller/>
         </div>
       </div >
     );
