16fd5d6e6d3baf46a92394f82aea3935d2d96b4b
Ansgar Mertens <a@amrtns.com>
2019-01-21 19:25:58 +0100

    add graphql function to proxy graph bahn endpoint

---
+++ functions/graphql.js
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+
+const API_ENDPOINT =
+  "https://bahnql.herokuapp.com/graphql";
+
+exports.handler = async (event, context) => {
+  try {
+
+    console.log(event.body);
+
+    return fetch(API_ENDPOINT, { method: 'POST',
+      body: event.body,
+      headers: {'Content-Type': 'application/json', 'accept': '*/*'}
+    })
+      .then(response => {
+        console.log(response);
+        if (response.status !== 200) {
+          throw new Error('Error from bahnql endpoint')
+        }
+        return response.text();
+      })
+      .then(json => ({ statusCode: 200, body: json}))
+      .catch(error => ({ statusCode: 422, body: String(error) }));
+  } catch (e) {
+    console.log(e); 
+    return ({ statusCode: 500, body: String(error) })
+  }
+};
