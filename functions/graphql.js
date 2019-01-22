537a396aa47420adf53089dae941addfc6c30f99
Ansgar Mertens <a@amrtns.com>
2019-01-22 17:21:08 +0100

    disable fallback

--- functions/graphql.js
+++ functions/graphql.js
@@ -5,7 +5,7 @@ const API_ENDPOINT = "https://bahnql.herokuapp.com/graphql";
 const response_if_the_server_fails_again = "{\"data\":{\"routing\":[{\"parts\":[{\"from\":{\"name\":\"Hamburg Hbf\",\"__typename\":\"Station\"},\"to\":{\"name\":\"Frankfurt (Main) Hbf\",\"__typename\":\"Station\"},\"delay\":0,\"product\":{\"name\":\"ICE 773\",\"__typename\":\"Product\"},\"direction\":\"Stuttgart Hbf\",\"departingTrack\":{\"platform\":\"B06\",\"number\":\"14\",\"length\":438,\"height\":76,\"name\":\"Gleis 14\",\"__typename\":\"Track\"},\"arrivingTrack\":{\"platform\":\"B04\",\"number\":\"6\",\"length\":434,\"height\":76,\"name\":\"Gleis 6\",\"__typename\":\"Track\"},\"__typename\":\"RoutePart\"}],\"__typename\":\"Route\"}]}}";
 
 exports.handler = async (event, context) => {
-  return { statusCode: 200, body: response_if_the_server_fails_again };
+  // return { statusCode: 200, body: response_if_the_server_fails_again };
 
   try {
     console.log(event.body);
